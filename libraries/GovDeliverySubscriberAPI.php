dec91ed7917afa4b211d981bc9ca8cf1f7d39618
Sheldon Rampton <sheldon@sheldonrampton.com>
2013-09-09 01:50:42 -0400

    Issue #2079269 by Sheldon Rampton: add signup and taxonomy modules

--- libraries/GovDeliverySubscriberAPI.php
+++ libraries/GovDeliverySubscriberAPI.php
@@ -1,59 +1,61 @@
 <?php
 /**
  * @file
- * Defines the GovDeliverySubscriberAPI, which handles creating Topics and assigning them to Catgories
+ * Defines the GovDeliverySubscriberAPI, which handles creating Topics and
+ * assigning them to Catgories.
  */
 
 class GovDeliverySubscriberAPI {
-
-  public $govdelivery_account_name;
-  public $govdelivery_account_password;
-  public $govdelivery_url_base;
-  private $auth_header;
+  public $govdeliveryAccountName;
+  public $govdeliveryAccountPassword;
+  public $govdeliveryUrlBase;
+  protected $authHeader;
 
   /**
-   *  Constructor
+   * Constructor.
    */
-  function __construct($govdelivery_account_name, $govdelivery_account_password, $govdelivery_url_base) {
-    $this->govdelivery_account_name = $govdelivery_account_name;
-    $this->govdelivery_account_password = $govdelivery_account_password;
-    $this->govdelivery_url_base = $govdelivery_url_base;
+  public function __construct($govdelivery_account_name, $govdelivery_account_password, $govdelivery_url_base) {
+    $this->govdeliveryAccountName = $govdelivery_account_name;
+    $this->govdeliveryAccountPassword = $govdelivery_account_password;
+    $this->govdeliveryUrlBase = $govdelivery_url_base;
     // Login header:
     // Authorization: Basic base64_encode('username:password');
-    $this->auth_header = $this->govdelivery_account_name. ':' . $this->govdelivery_account_password;
+    $this->authHeader = $this->govdeliveryAccountName . ':' . $this->govdeliveryAccountPassword;
   }
 
   /**
-   *  Take a topic item from the queue and create it in GovDelivery
+   * Take a topic item from the queue and create it in GovDelivery.
    *
-   *  @param $topic Topic object
+   * @param object $topic
+   *   Topic object.
    *
-   *  @return array of return object from topic creation
+   * @return array
+   *   Return object from topic creation.
    */
-  function create_topic($topic) {
-    $url = $this->govdelivery_url_base . '/topics.xml';
+  public function createTopic($topic) {
+    $url = $this->govdeliveryUrlBase . '/topics.xml';
 
     if (!isset($topic->topic_id) || is_null($topic->topic_id)) {
       throw new Exception(t('topic_id attribute of supplied topic is unset or null.  You must supply a topic_id'));
     }
 
-    // GovDelivery Subscriber API requires al topic_id's to be be 32 characers or less
+    // GovDelivery Subscriber API requires topic IDs to be 32 characers or less.
     $id_length = strlen($topic->topic_id);
     if ($id_length > 32) {
-      throw new Exception(t('topic_id must be 32 characters or less. Supplied id of %id is %length characters', array('%id' => $topic->topic_id, '%length' => $id_length)));  
+      throw new Exception(t('topic_id must be 32 characters or less. Supplied id of %id is %length characters', array('%id' => $topic->topic_id, '%length' => $id_length)));
     }
 
-    // GovDelivery Subscriber API requires all topic_id's to be all CAPS. 
+    // GovDelivery Subscriber API requires all topic_id's to be all CAPS.
     $topic->topic_id = strtoupper($topic->topic_id);
 
     $create_topic_xml_request = '
       <?xml version="1.0" encoding="UTF-8"?>
       <topic>
-         <code type="string">'. $topic->topic_id  .'</code>
+         <code type="string">' . $topic->topic_id  . '</code>
          <default-pagewatch-results type="integer" nil="true"></default-pagewatch-results>
          <description nil="true"></description>
          <lock-version type="integer">0</lock-version>
-         <name>'. $topic->title  .'</name>
+         <name>' . $topic->title  . '</name>
          <pagewatch-autosend type="boolean">false</pagewatch-autosend>
          <pagewatch-enabled type="boolean">false</pagewatch-enabled>
          <pagewatch-suspended type="boolean">false</pagewatch-suspended>
@@ -61,7 +63,7 @@ class GovDeliverySubscriberAPI {
          <rss-feed-title>Topic Name - Recent Updates</rss-feed-title>
          <rss-feed-url>http://url_for_rss_feed.rss</rss-feed-url>
          <send-by-email-enabled type="boolean">false</send-by-email-enabled>
-         <short-name>'. $topic->short_title .'</short-name>
+         <short-name>' . $topic->short_title . '</short-name>
          <subscribers-count type="integer">0</subscribers-count>
          <wireless-enabled type="boolean">false</wireless-enabled>
          <pages type="array">
@@ -72,41 +74,45 @@ class GovDeliverySubscriberAPI {
          <visibility>2</visibility>
          </topic>';
 
-    // Set options
+    // Set options.
     $topic_options = array(
       CURLOPT_URL => $url,
       CURLOPT_FAILONERROR => TRUE,
       CURLOPT_RETURNTRANSFER => TRUE,
       CURLOPT_TIMEOUT => 120,
-      CURLOPT_USERPWD => $this->auth_header,
+      CURLOPT_USERPWD => $this->authHeader,
       CURLOPT_HEADER => TRUE,
-      CURLOPT_HTTPHEADER => array('Content-Type: application/xml',),
+      CURLOPT_HTTPHEADER => array('Content-Type: application/xml'),
       CURLOPT_POST => TRUE,
       CURLOPT_POSTFIELDS => $create_topic_xml_request,
     );
 
-    $ch = curl_init();    // create curl resource
+    // Create curl resource.
+    $ch = curl_init();
     curl_setopt_array($ch, $topic_options);
 
-    $topic_return = new stdClass;
-    $topic_return->response = curl_exec($ch); // execute and get response
-    $topic_return->header   = substr($topic_return->response, 0, curl_getinfo($ch, CURLINFO_HEADER_SIZE));
-    $topic_return->body     = substr($topic_return->response, curl_getinfo($ch, CURLINFO_HEADER_SIZE));
-    $topic_return->error    = curl_error($ch);
-    $topic_return->info     = curl_getinfo($ch);
+    $topic_return = new stdClass();
+    // Execute and get response.
+    $topic_return->response = curl_exec($ch);
+    $topic_return->header = substr($topic_return->response, 0, curl_getinfo($ch, CURLINFO_HEADER_SIZE));
+    $topic_return->body = substr($topic_return->response, curl_getinfo($ch, CURLINFO_HEADER_SIZE));
+    $topic_return->error = curl_error($ch);
+    $topic_return->info = curl_getinfo($ch);
     $topic_return->topic_id = $topic->topic_id;
 
     return $topic_return;
   }
 
   /**
-   *  Update the GovDelivery category associated with the given topic
+   * Update the GovDelivery category associated with the given topic.
    *
-   *  @param $topic  Topic object
+   * @param object $topic
+   *   Topic object.
    *
-   *  @return array of return object from category updating
+   * @return array
+   *   Return object from category updating.
    */
-  function update_topic_category($topic) { 
+  public function updateTopicCategory($topic) {
     if (!isset($topic->topic_id) || is_null($topic->topic_id)) {
       throw new exception(t('topic_id attribute of supplied topic is unset or null.  you must supply a topic_id'));
     }
@@ -115,7 +121,7 @@ class GovDeliverySubscriberAPI {
       throw new exception(t('category_id attribute of supplied topic is unset or null.  you must supply a category_id'));
     }
 
-    $url = $this->govdelivery_url_base . '/topics/' . $topic->topic_id . '/categories.xml';
+    $url = $this->govdeliveryUrlBase . '/topics/' . $topic->topic_id . '/categories.xml';
     $set_category_xml_request = '
         <?xml version="1.0" encoding="UTF-8"?>
         <topic>
@@ -126,38 +132,40 @@ class GovDeliverySubscriberAPI {
           </categories>
         </topic>';
 
-    //write the request out to a tmp file since PUT isn't happy taking a string
+    // Write the request to a tmp file since PUT isn't happy taking a string.
     $fp = fopen('php://memory', 'w+');
     fwrite($fp, $set_category_xml_request);
     fseek($fp, 0);
 
-    // Set options 
+    // Set options.
     $category_options = array(
       CURLOPT_URL => $url,
       CURLOPT_FAILONERROR => TRUE,
       CURLOPT_RETURNTRANSFER => TRUE,
       CURLOPT_TIMEOUT => 30,
-      CURLOPT_USERPWD => $this->auth_header,
+      CURLOPT_USERPWD => $this->authHeader,
       CURLOPT_HEADER => TRUE,
-      CURLOPT_HTTPHEADER => array('Content-Type: application/xml',),
+      CURLOPT_HTTPHEADER => array('Content-Type: application/xml'),
       CURLOPT_PUT => TRUE,
       CURLOPT_INFILE => $fp,
       CURLOPT_INFILESIZE => strlen($set_category_xml_request),
     );
 
-    $ch = curl_init();    // create curl resource
+    // Create curl resource.
+    $ch = curl_init();
     curl_setopt_array($ch, $category_options);
 
-    $category_return = new stdClass;
-    $category_return->response = curl_exec($ch); // execute and get response
-    $category_return->header   = substr($category_return->response, 0, curl_getinfo($ch, CURLINFO_HEADER_SIZE));
-    $category_return->body     = substr($category_return->response, curl_getinfo($ch, CURLINFO_HEADER_SIZE));
-    $category_return->error    = curl_error($ch);
-    $category_return->info     = curl_getinfo($ch);
+    // Execute and get response.
+    $category_return = new stdClass();
+    $category_return->response = curl_exec($ch);
+    $category_return->header = substr($category_return->response, 0, curl_getinfo($ch, CURLINFO_HEADER_SIZE));
+    $category_return->body = substr($category_return->response, curl_getinfo($ch, CURLINFO_HEADER_SIZE));
+    $category_return->error = curl_error($ch);
+    $category_return->info = curl_getinfo($ch);
 
-    //kill the file stream we created above
+    // Kill the file stream we created above.
     fclose($fp);
     curl_close($ch);
-    return $category_return; 
+    return $category_return;
   }
 }
--- libraries/odmService.php
+++ libraries/odmService.php
@@ -1,15 +1,28 @@
 <?php
-
 /**
  * @file
  * ODM Services for the GovDelivery Integration module.
  */
 
+/**
+ * Base class for other classes representing XML data.
+ */
 class XMLData {
+  /**
+   * Replaces XML special characters.
+   *
+   * @param string $data
+   *   XML data.
+   *
+   * @return string
+   *   UTF8 XML string.
+   */
   public function xmlSpecialChars($data) {
-    $strict = TRUE; // Strict checking is required for mb_detect to be useful.
-    $isEndcoded = mb_detect_encoding($data, array('UTF-8'), $strict); // Empty string if it's not in the list of encodings to check.
-    if(!empty($isEncoded)) {
+    // Strict checking is required for mb_detect to be useful.
+    $strict = TRUE;
+    // Empty string if it's not in the list of encodings to check.
+    $is_encoded = mb_detect_encoding($data, array('UTF-8'), $strict);
+    if (!empty($is_encoded)) {
       return preg_replace('@[\x00-\x08\x0B\x0C\x0E-\x1F]@', ' ', $data);
     }
 
@@ -18,31 +31,61 @@ class XMLData {
 }
 
 /**
- * Please use the constructor or the setters to prevent invalid characters from making it into a SOAP request.
+ * Please use the constructor or the setters to prevent invalid characters
+ * from making it into a SOAP request.
  */
 class ODMCredentials extends XMLData {
   public $realm = 'ODM';
   public $username;
   public $password;
 
+  /**
+   * Constructor.
+   *
+   * @param string $username
+   *   Username.
+   * @param string $password
+   *   Password.
+   */
   public function __construct($username, $password) {
     $this->username = parent::xmlSpecialChars($username);
     $this->password = parent::xmlSpecialChars($password);
   }
 
+  /**
+   * Set the realm.
+   *
+   * @param string $data
+   *   Realm data.
+   */
   public function setRealm($data) {
     $this->realm = parent::xmlSpecialChars($data);
   }
 
+  /**
+   * Set username.
+   *
+   * @param string $username
+   *   Username.
+   */
   public function setUsername($username) {
     $this->username = parent::xmlSpecialChars($username);
   }
 
+  /**
+   * Set password.
+   *
+   * @param string $password
+   *   Password.
+   */
   public function setPassword($password) {
     $this->password = parent::xmlSpecialChars($password);
   }
 }
 
+/**
+ * ODM message.
+ */
 class ODMMessage extends XMLData {
   public $body;
   public $emailColumn = 'email';
@@ -55,78 +98,134 @@ class ODMMessage extends XMLData {
 
 
   /*public function __set($name, $value) {
-    watchdog('ben', 'CALLED SET');
-    $this->$name = parent::xmlSpecialChars($value);
+  watchdog('ben', 'CALLED SET');
+  $this->$name = parent::xmlSpecialChars($value);
   }*/
 
+  /**
+   * Set body.
+   *
+   * @param string $body
+   *   Body.
+   */
   public function setBody($body) {
     $this->body = parent::xmlSpecialChars($body);
   }
 
-  public function setEmailColumn($emailColumn) {
-    $this->emailColumn = parent::xmlSpecialChars($emailColumn);
+  /**
+   * Set email column.
+   *
+   * @param string $email_column
+   *   Email column.
+   */
+  public function setEmailColumn($email_column) {
+    $this->emailColumn = parent::xmlSpecialChars($email_column);
   }
 
-  public function setFromName($fromName) {
-    $this->fromName = parent::xmlSpecialChars($fromName);
+  /**
+   * Set from name.
+   *
+   * @param string $from_name
+   *   From name.
+   */
+  public function setFromName($from_name) {
+    $this->fromName = parent::xmlSpecialChars($from_name);
   }
 
-  public function setRecordDesignator($recordDesignator) {
-    $this->recordDesignator = parent::xmlSpecialChars($recordDesignator);
+  /**
+   * Set record designator.
+   *
+   * @param string $record_designator
+   *   Record designator.
+   */
+  public function setRecordDesignator($record_designator) {
+    $this->recordDesignator = parent::xmlSpecialChars($record_designator);
   }
 
+  /**
+   * Set subject.
+   *
+   * @param string $subject
+   *   Subject.
+   */
   public function setSubject($subject) {
     $this->subject = parent::xmlSpecialChars($subject);
   }
 }
 
+/**
+ * Delivery activity.
+ */
 class ODMDeliveryActivitySince extends XMLData {
   public $maxresults;
   public $sequence = '';
 
+  /**
+   * Set sequence.
+   *
+   * @param string $sequence
+   *   Sequence.
+   */
   public function setSequence($sequence) {
     $this->sequence = parent::xmlSpecialChars($sequence);
   }
 }
 
 /**
- * ODMv2 Service class
- *
+ * ODMv2 Service class.
  */
-class odmService extends SoapClient {
-  public $server_uri = 'http://odm.govdelivery.com/ODMv2';
+class OdmService extends SoapClient {
+  public $serverUri = 'http://odm.govdelivery.com/ODMv2';
 
-  private static $classmap = array(
+  protected static $classmap = array(
     'credentials' => 'ODMCredentials',
     'message' => 'ODMMessage',
     'deliveryActivitySince' => 'ODMDeliveryActivitySince',
   );
 
-  public function odmService($wsdl, ODMCredentials $credentials, $options = array()) {
-   foreach (self::$classmap as $key => $value) {
+  /**
+   * Constructor.
+   *
+   * @param string $wsdl
+   *   WDSL data.
+   * @param ODMCredentials $credentials
+   *   Credentials.
+   * @param array $options
+   *   Options.
+   */
+  public function __construct($wsdl, ODMCredentials $credentials, $options = array()) {
+    foreach (self::$classmap as $key => $value) {
       if (!isset($options['classmap'][$key])) {
         $options['classmap'][$key] = $value;
       }
     }
 
     // @todo must be used if runtime debugging feature is added.
-    $options['trace'] = true;
-
+    $options['trace'] = TRUE;
     $this->setCredentialsHeader($credentials);
 
     parent::__construct($wsdl, $options);
-
   }
 
-  private function setCredentialsHeader(ODMCredentials $credentials) {
+  /**
+   * Set credentials header.
+   *
+   * @param ODMCredentials $credentials
+   *   Credentials.
+   */
+  protected function setCredentialsHeader(ODMCredentials $credentials) {
     // Generate the needed credential headers.
     // @todo SimpleXMLElement::getDocNamespaces
-    $header = new SoapHeader($this->server_uri,                           // Setup the correct namespace...I can't find a good way to query the base namespace from the wsdl. 
-                             'credentials',                               // Encapsulating tag.
-                             new SoapVar($credentials, SOAP_ENC_OBJECT)); // Generates an unencapsulated set of xml tags and their values. 
+    // Setup the correct namespace...I can't find a good way to query the
+    // base namespace from the wsdl.
+    // Encapsulating tag.
+    // Generates an unencapsulated set of xml tags and their values.
+    $header = new SoapHeader($this->serverUri,
+                             'credentials',
+                             new SoapVar($credentials, SOAP_ENC_OBJECT));
 
     // Set the credentials to the header section of the soap envelope.
-    $this->__setSoapHeaders($header); 
+    $this->__setSoapHeaders($header);
   }
 
   /**
@@ -134,25 +233,32 @@ class odmService extends SoapClient {
    *
    * @todo Add a runtime debugging switch.
    *
-   * @param ODMCredentials $credentials
    * @param ODMMessage $message
-   * @return mixed
+   *   The message.
+   *
+   * @return int
    *   Returns string on success and null or SoapFault exception on failure.
    */
   public function sendMessage(ODMMessage $message) {
-    // Use the autogenerated base method that SoapClient class generates when using a valid WSDL.
+    // Use the autogenerated base method that SoapClient class generates
+    // when using a valid WSDL.
     $result = parent::sendMessage($message);
     return $result;
   }
 
-  public function deliveryActivitySince (ODMDeliveryActivitySince $deliveryActivitySince) {
-    $result = parent::deliveryActivitySince($deliveryActivitySince);
+  /**
+   * Handle delivery activity.
+   *
+   * @param ODMDeliveryActivitySince $delivery_activity_since
+   *   Activity data.
+   */
+  public function deliveryActivitySince(ODMDeliveryActivitySince $delivery_activity_since) {
+    $result = parent::deliveryActivitySince($delivery_activity_since);
 
-/*    $handle = fopen('/tmp/govd_odmv2.tmp', 'a+');
+    /* $handle = fopen('/tmp/govd_odmv2.tmp', 'a+');
     fwrite($handle, 'Last Request' . PHP_EOL . $this->__getLastRequest());
     fclose($handle);
- */
-
-    return $result; 
+    */
+    return $result;
   }
 }
